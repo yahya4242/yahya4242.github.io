8616b18d703dc1e6dd5a88f89753a78bc9ba0ca2
yahya4242 <yahya.mousa4242@gmail.com>
2020-09-05 10:27:39 +0200

    done!

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready (function (){
+
+$('#portfolio-button').click(function(){
+        $('html, body').animate({
+            scrollTop: $('#portfolio-section').offset().top
+        }, 1500);
+    });
+});
